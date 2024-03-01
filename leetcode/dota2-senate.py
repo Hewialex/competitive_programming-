181a76b3d93736c386549a83d796f97e356e9a4c
Hewialex <123300353+Hewialex@users.noreply.github.com>
2024-03-01 04:58:54 -0700

    Add solution for Dota2 Senate

---
+++ leetcode/dota2-senate.py
@@ -0,0 +1,20 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        qr = deque()
+        qd = deque()
+        for i , senator in enumerate(senate):
+            if senator == 'R':
+                qr.append(i)  
+            else:
+                qd.append(i)
+        n = len(senate)
+        while qr and qd:
+            if qr[0] < qd[0]:
+                qr.append(qr[0] + n)
+            else:
+                qd.append(qd[0] + n)
+            qr.popleft()
+            qd.popleft()
+        
+
+        return "Radiant" if qr else "Dire"
