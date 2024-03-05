e7e137dabb7eae48aa52831f7935f23bfa03dc03
Hewialex <123300353+Hewialex@users.noreply.github.com>
2024-03-05 09:11:15 -0700

    Add solution for Subsets II

---
+++ leetcode/subsets-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()  
+        res = []
+        path = []
+
+        def backtrack(i):
+            res.append(path[:])
+
+            for j in range(i, len(nums)):
+                if j > i and nums[j] == nums[j - 1]:
+                    continue
+                path.append(nums[j])
+                backtrack(j + 1)
+                path.pop()
+
+        backtrack(0)
+        return res
